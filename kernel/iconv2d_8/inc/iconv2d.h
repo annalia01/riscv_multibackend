c282982620939da3c2ab4edf35373da3fea5b3e5
annalia01 <annalia.ruggiero01@gmail.com>
2025-11-06 11:22:12 +0100

    Update iconv2d.h

--- kernel/iconv2d_8/inc/iconv2d.h
+++ kernel/iconv2d_8/inc/iconv2d.h
@@ -24,7 +24,7 @@
 #include <stdint.h>
 
 
-void iconv2d_3x3_uint8(int8_t *o, uint8_t *i, int8_t *f, int64_t R, int64_t C,
+void iconv2d_3x3_uint8(uint8_t *o, uint8_t *i, uint8_t *f, int64_t R, int64_t C,
                  int64_t F);
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))

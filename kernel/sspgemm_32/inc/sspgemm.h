f4841f9df3ecee3fb10f61e44c937c37c10817fb
annalia01 <annalia.ruggiero01@gmail.com>
2025-11-13 18:25:18 +0100

    Update sspgemm.h

--- kernel/sspgemm_32/inc/sspgemm.h
+++ kernel/sspgemm_32/inc/sspgemm.h
@@ -1,20 +1,14 @@
-// Copyright 2022 ETH Zurich and University of Bologna.
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Author: Chi Zhang, ETH Zurich <chizhang@iis.ee.ethz.ch>
+/*
+ * Vector kernel implemented for the thesis work of <Annalia Ruggiero>.
+ *
+ * The algorithm is based on the S2 scheme described in:
+ * <Titopoulos; Alexandridis; Peltekis; Nicopoulos; Dimitrakopoulos>,
+ * <Optimizing Structured-Sparse Matrix Multiplication in RISC-V Vector Processors>, <2025>.
+ *
+ * In this implementation, non-zero values are stored following
+ * the layout used in algorithm S3, while the computational flow
+ * follows algorithm S2.
+ */
 
 #ifndef _SSPGEMM_H
 #define _SSPGEMM_H

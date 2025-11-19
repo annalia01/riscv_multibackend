b1bd20ea64e99b543da4d9a088ba305302b3bf44
annalia01 <annalia.ruggiero01@gmail.com>
2025-11-19 09:31:48 +0100

    aggiunta kernel

---
+++ kernel/vgg-16_8/inc/max_pool.h
@@ -0,0 +1,8 @@
+#ifndef MAX_POOL_H
+#define MAX_POOL_H
+
+#include <stdint.h>
+
+void maxpool2x2(const int8_t *in, int H, int W, int8_t *out);
+
+#endif
---
+++ kernel/vgg-16_8/src/fc.c
@@ -0,0 +1,37 @@
+#include <stdint.h>
+#include <stddef.h>
+
+
+void fc(int8_t *mat, int8_t *vec, int rows, int cols, int8_t *out_vec)
+{
+    for(int r = 0; r < rows; r++) {
+
+        int8_t *row_ptr = mat + r * cols;
+        int8_t acc = 0;
+
+        int c = 0;
+        while(c < cols) {
+
+            size_t vl;
+            asm volatile("vsetvli %0, %1, e8, m8, ta, ma"
+                        : "=r"(vl)
+                        : "r"(cols - c));
+            asm volatile("vle8.v v0, (%0)" :: "r"(row_ptr + c));
+
+            asm volatile("vle8.v v8, (%0)" :: "r"(vec + c));
+
+            asm volatile("vmul.vv v16, v0, v8");
+
+            asm volatile("vmv.v.i v24, 0");
+            asm volatile("vredsum.vs v24, v16, v24");
+
+            int8_t tmp;
+            asm volatile("vmv.x.s %0, v24" : "=r"(tmp));
+
+            acc += tmp;
+            c += vl;
+        }
+
+        out_vec[r] = acc;
+    }
+}

fe8315688c4c0fabc1234e27923ec03642fd4c0e
annalia01 <annalia.ruggiero01@gmail.com>
2025-11-20 14:37:01 +0100

    Update relu.c

--- kernel/mobilenet_32/src/relu.c
+++ kernel/mobilenet_32/src/relu.c
@@ -9,21 +9,15 @@ void relu(int32_t *data, int N) {
 
         size_t vl;
 
-        asm volatile("vsetvli %0, %1, e32, m8, ta, ma"
-                     : "=r"(vl)
-                     : "r"(N - idx));
+        asm volatile("vsetvli %0, %1, e32, m8, ta, ma" : "=r"(vl): "r"(N - idx));
 
-        asm volatile("vle32.v v0, (%0)"
-                     :
-                     : "r"(data + idx));
+        asm volatile("vle32.v v0, (%0)" :: "r"(data + idx));
 
         asm volatile("vmv.v.i v8, 0");
 
         asm volatile("vmax.vv v16, v0, v8");
 
-        asm volatile("vse32.v v16, (%0)"
-                     :
-                     : "r"(data + idx));
+        asm volatile("vse32.v v16, (%0)" :: "r"(data + idx));
 
         idx += vl;
     }

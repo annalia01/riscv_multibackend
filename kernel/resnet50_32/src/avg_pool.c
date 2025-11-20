c3d095b9004ff1d900046d724eeaa89cc5718cc5
annalia01 <annalia.ruggiero01@gmail.com>
2025-11-20 14:31:38 +0100

    Update avg_pool.c

--- kernel/resnet50_32/src/avg_pool.c
+++ kernel/resnet50_32/src/avg_pool.c
@@ -25,27 +25,18 @@ void avgpool2x2(const int32_t *in, int H, int W, int32_t *out)
 
             size_t vl;
 
-           
-            asm volatile("vsetvli %0, %1, e32, m4, ta, ma"
-                        : "=r"(vl)
-                        : "r"(2));
-
-            
+            asm volatile("vsetvli %0, %1, e32, m8, ta, ma" : "=r"(vl) : "r"(2));
             asm volatile("vle32.v v0, (%0)" :: "r"(p0));
+            asm volatile("vle32.v v8, (%0)" :: "r"(p1));
 
-            
-            asm volatile("vle32.v v4, (%0)" :: "r"(p1));
-
-            
-            asm volatile("vadd.vv v8, v0, v4");
+            asm volatile("vadd.vv v16, v0, v8");
             printf("...\n");
             
-            asm volatile("vmv.v.i v12, 0");
-            asm volatile("vredsum.vs v12, v8, v12");
+            asm volatile("vmv.v.i v24, 0");
+            asm volatile("vredsum.vs v24, v16, v24");
 
             int32_t sum;
-            asm volatile("vmv.x.s %0, v12" : "=r"(sum));
-
+            asm volatile("vmv.x.s %0, v24" : "=r"(sum));
 
             out_row[c] = sum/4;
         }

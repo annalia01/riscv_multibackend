8eba47e271fa959e421b115ea5f2f9e8fb4717ae
annalia01 <annalia.ruggiero01@gmail.com>
2025-11-22 11:18:03 +0100

    Update dataset.c

--- data/resnet50_8/dataset.c
+++ data/resnet50_8/dataset.c
@@ -4,68 +4,78 @@
 #define NR_LANES 8
 #endif
 
-#define IN_H 16
-#define IN_W 16
 
-#define CONV_F 3
-#define CONV1_OUT_H (IN_H - 2)          // 14
-#define CONV1_OUT_W (IN_W - 2)          // 14
+#define IN_H 224
+#define IN_W 224
 
-#define POOL1_OUT_H (CONV1_OUT_H / 2)   // 7
-#define POOL1_OUT_W (CONV1_OUT_W / 2)   // 7
+#define CONV7_F 7
+#define CONV1_OUT_H (IN_H - CONV7_F + 1)   
+#define CONV1_OUT_W (IN_W - CONV7_F + 1)   
 
-#define CONV2_OUT_H (POOL1_OUT_H - 2)    // 5
-#define CONV2_OUT_W (POOL1_OUT_W - 2)    // 5
+// MaxPool 2x2
+#define POOL1_OUT_H (CONV1_OUT_H / 2)      // 109
+#define POOL1_OUT_W (CONV1_OUT_W / 2)      // 109
 
-#define POOL2_OUT_H (CONV2_OUT_H / 2)    // 2
-#define POOL2_OUT_W (CONV2_OUT_W / 2)    // 2
 
-#define FLAT_SIZE (POOL2_OUT_H * POOL2_OUT_W)   // 4
+#define FINAL_H 1
+#define FINAL_W 1
+#define FLAT_SIZE (FINAL_H * FINAL_W)      
+
 #define FC_OUT 10
 
 
-int8_t input_image[IN_H * IN_W]
-    __attribute__((aligned(32*NR_LANES))) = {
-    1,2,3,4,5,6,7,8, 9,10,11,12,13,14,15,16,
-    2,3,4,5,6,7,8,9, 10,11,12,13,14,15,16,1,
-    3,4,5,6,7,8,9,10, 11,12,13,14,15,16,1,2,
-    4,5,6,7,8,9,10,11, 12,13,14,15,16,1,2,3,
-    5,6,7,8,9,10,11,12, 13,14,15,16,1,2,3,4,
-    6,7,8,9,10,11,12,13, 14,15,16,1,2,3,4,5,
-    7,8,9,10,11,12,13,14, 15,16,1,2,3,4,5,6,
-    8,9,10,11,12,13,14,15, 16,1,2,3,4,5,6,7,
 
-    9,10,11,12,13,14,15,16, 1,2,3,4,5,6,7,8,
-    10,11,12,13,14,15,16,1, 2,3,4,5,6,7,8,9,
-    11,12,13,14,15,16,1,2, 3,4,5,6,7,8,9,10,
-    12,13,14,15,16,1,2,3, 4,5,6,7,8,9,10,11,
-    13,14,15,16,1,2,3,4, 5,6,7,8,9,10,11,12,
-    14,15,16,1,2,3,4,5, 6,7,8,9,10,11,12,13,
-    15,16,1,2,3,4,5,6, 7,8,9,10,11,12,13,14,
-    16,1,2,3,4,5,6,7, 8,9,10,11,12,13,14,15
+int8_t input_image[IN_H * IN_W]
+    __attribute__((aligned(32 * NR_LANES)));
+
+
+
+
+int8_t filter_7x7[49]
+    __attribute__((aligned(32 * NR_LANES))) = {
+     1, 0, -1, 1, 0, -1, 1,
+     1, 0, -1, 1, 0, -1, 1,
+     1, 0, -1, 1, 0, -1, 1,
+     1, 0, -1, 1, 0, -1, 1,
+     1, 0, -1, 1, 0, -1, 1,
+     1, 0, -1, 1, 0, -1, 1,
+     1, 0, -1, 1, 0, -1, 1
 };
+
 
 
 int8_t filter_3x3[9]
-    __attribute__((aligned(32*NR_LANES))) = {
-    1, 0, -1,
-    1, 0, -1,
-    1, 0, -1
+    __attribute__((aligned(32 * NR_LANES))) = {
+     1,  0, -1,
+     1,  0, -1,
+     1,  0, -1
 };
 
+
+
+
 int8_t fc_weights[FLAT_SIZE * FC_OUT]
-    __attribute__((aligned(32*NR_LANES)));
+    __attribute__((aligned(32 * NR_LANES)));
 
 int8_t fc_bias[FC_OUT]
-    __attribute__((aligned(32*NR_LANES)));
+    __attribute__((aligned(32 * NR_LANES)));
 
 
-__attribute__((constructor))
-static void init_fc()
+
+
+static void init_dataset()
 {
-    for (int i = 0; i < FLAT_SIZE * FC_OUT; i++)
-        fc_weights[i] = (i % 4) - 2;   
+    
+    for (int r = 0; r < IN_H; r++) {
+        for (int c = 0; c < IN_W; c++) {
+            
+            input_image[r * IN_W + c] = (r * 131 + c * 17) % 256;
+        }
+    }
 
+    for (int i = 0; i < FC_OUT; i++)
+        fc_weights[i] = (i % 5) - 2;   
+    
     for (int j = 0; j < FC_OUT; j++)
-        fc_bias[j] = 0;
+        fc_bias[j] = j; 
 }
